0de46ec281c7f9f70cfe53fe48e8fa16f85a8420
scharlez <stevencharlez@gmail.com>
2022-08-09 16:46:40 -0700

    Completed Home Page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Home from './Home'
 
-export default class Home extends Component {
+class Home extends Component {
+  // constructor(props) {
+  //   super(props); 
+  //   this.state = {
+
+  //   }
+  // }
   render() {
     return (
-      <h3>Home</h3>
+      <>
+        <div style={{display:'flex', justifyContent:'center'}}>
+        <h2>Welcome to Apartment Hunter</h2>
+        </div>
+        <br></br>
+        <div style={{display:'flex', justifyContent:'center'}}>
+        <p>Find your new home today!</p>
+        <br></br>
+        <Button color="primary">Sign-In</Button>
+        <Button color="primary">Sign-Up</Button>
+        </div>
+      </>
     )
   }
 }
+
+
+
+
+
+export default Home; 
