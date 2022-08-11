b422f87e32fa2354803c1622d14988deb3c9d48b
Genemichael25 <genemichael25@yahoo.com>
2022-08-11 13:28:47 -0700

    Created tests for each attribute

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -31,20 +31,22 @@ RSpec.describe "Apartments", type: :request do
     end
   
 
-    it "doesn't create an apartment without valid attributes" do
+    it "doesn't create an apartment without valid street" do
+
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
       apartment_params = {
         apartment: {
-          street: nil,
-          city: nil,
-          state: nil,
-          manager: nil,
-          email: nil, 
-          price: nil, 
-          bedrooms: nil, 
-          bathrooms: nil, 
-          pets: nil,
-          image: nil,
-          user_id:nil
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 5,
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
         }
       }
       
@@ -53,22 +55,272 @@ RSpec.describe "Apartments", type: :request do
       expect(response.status).to eq 422
       json = JSON.parse(response.body)
       expect(json['street']).to include "can't be blank"
+    end
+
+    it "doesn't create an apartment without valid city" do
+
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 5,
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['city']).to include "can't be blank"
+    end
+
+    it "doesn't create an apartment without valid state" do
+
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 5,
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['state']).to include "can't be blank"
+    end
+      
+    it "doesn't create an apartment without valid manager" do
+
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 5,
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['manager']).to include "can't be blank"
+    end
+      
+    it "doesn't create an apartment without valid email" do
+
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          price: '$1,675,800',
+          bedrooms: 5,
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['email']).to include "can't be blank"
+    end
+      
+    it "doesn't create an apartment without valid price" do
+
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          bedrooms: 5,
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['price']).to include "can't be blank"
+    end
+      
+    
+    it "doesn't create an apartment without valid bedrooms" do
+      
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+      
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bathrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['bedrooms']).to include "can't be blank"
+    end
+      
+    it "doesn't create an apartment without valid bathrooms" do
+      
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+      
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 2,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['bathrooms']).to include "can't be blank"
+    end
+      
+    it "doesn't create an apartment without valid pets" do
+      
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+      
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 2,
+          bathrooms: 1,
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['pets']).to include "can't be blank"
+    end
+      
+    it "doesn't create an apartment without valid image" do
+      
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+      
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 2,
+          bathrooms: 1,
+          pets: 'No',
+          user_id: user1.id
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['image']).to include "can't be blank"
+    end
+
+    it "doesn't create an apartment without valid user id" do
+      
+      user1 = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+      
+      apartment_params = {
+        apartment: {
+          street: '1329 Carroll Avenue',
+          city: 'Los Angeles',
+          state: 'CA',
+          manager: 'Alyssa Milano',
+          email: 'test1@test.com',
+          price: '$1,675,800',
+          bedrooms: 2,
+          bathrooms: 1,
+          pets: 'No',
+          image: 'https://photos.zillowstatic.com/fp/7f7209dfbb519e5aa13446bbbd60c43d-cc_ft_576.webp'
+        }
+      }
+      
+      post '/apartments', params: apartment_params
+      
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
       expect(json['user_id']).to include "can't be blank"
     end
-  end
-
-
-
-  describe "GET /index" do
+end
+      
+    
+    describe "GET /index" do
     it "returns a list of apartments" do
       user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
 
